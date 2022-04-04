9f2bfac573ecb85cf24d7731be54d38c779c1242
EldarK-NS <eldark.ns@gmail.com>
2022-04-04 10:07:29 +0500

    first

---
+++ src/hooks/redux.ts
@@ -0,0 +1,5 @@
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+
+const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
